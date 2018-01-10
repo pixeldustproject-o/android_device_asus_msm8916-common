d145cf9401f52bed51691011b61309a6799e7baf
Mrinal Ghosh <31700322+mg712702@users.noreply.github.com>
2018-01-11 01:03:51 +0530

    Update BoardConfigCommon.mk (#1)
    Removed sepolicy includes.
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,12 +158,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)/releasetools
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 USE_OPENGL_RENDERER := true
 
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
-
 # Tap-to-Wake
 TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/i2c-5/5-0038/dclick_mode"
 
